a48e7b6999d0c89d93c522a030520d8d7a539f3e
Patrick <binary_thinker@yahoo.com>
2025-02-04 13:28:49 -0800

    Service is working.

--- app/src/main/java/edu/csueb/Service/DelayedMessageService.java
+++ app/src/main/java/edu/csueb/Service/DelayedMessageService.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.Context;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
@@ -14,6 +17,9 @@ import android.util.Log;
  * helper methods.
  */
 public class DelayedMessageService extends IntentService {
+
+    private Timer timer;
+    public static final String EXTRA_MESSAGE = "Trans Women is the EXTRA_MESSAGE";
 
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
@@ -58,43 +64,51 @@ public class DelayedMessageService extends IntentService {
         context.startService(intent);
     }
 
-    public static final String EXTRA_MESSAGE = "Trans Women";
-
+    /**
+     * onHandleIntent():
+     * ...should contain the code you want to run each time the service is passed an intent.
+     * It runs in a separate thread.
+     * If it's passed multiple intentions, it deals with them on at a time.
+     *
+     * @param intent
+     */
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.i("log", "onHandleIntent(Intent intent)");
+        Log.i("log", "Timer started...");
+        startTimer();
 
+        /*
         synchronized (this) {
             try {
                 wait(10000);
-                Log.i("log", "synchronized(this)");
+                Log.i("log", "* * * * * * * * * * * * * * * * * * * * ");
+                Log.i("log", "Service Message after 10000 milliseconds");
+                Log.i("log", "* * * * * * * * * * * * * * * * * * * * ");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        showText(intent.getStringExtra(EXTRA_MESSAGE));
+        */
     }
 
-    public void showText(final String name) {
-        Log.i("ShowText => ", name);
+
+    private int numberOfSeconds() {
+        return (1000 * 10);
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    private void startTimer() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Log.i("log", "* * * * * * * * * * * * * * * * * * * * ");
+                Log.i("log", "Service Message after 10000 milliseconds");
+                Log.i("log", "* * * * * * * * * * * * * * * * * * * * ");
+            }
+        };
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+        timer = new Timer(true);
+        int delay = numberOfSeconds();
+        int interval = numberOfSeconds();
+        timer.schedule(task, delay, interval);
     }
 }
--- app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
@@ -1,17 +1,24 @@
 package edu.csueb.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import edu.csueb.R;
+import edu.csueb.Service.DelayedMessageService;
 import edu.csueb.databinding.FragmentNotificationsBinding;
+import edu.csueb.ui.dashboard.DashboardViewModel;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
@@ -26,11 +33,10 @@ public class NotificationsFragment extends Fragment {
                 public void onClick(View view) {
                     binding.tvMessage.setText("Binding at work");
                     binding.serviceBtn.setText("Service Started");
-                    /*
-                    Intent intent = new Intent(getActivity(), DashboardViewModel.class);
-                    intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.app_name));
-                    startActivity(intent);
-                    */
+                    Intent intent = new Intent(getActivity(), DelayedMessageService.class);
+                    Log.i("log", "Inside of notificationsViewModel()");
+                    intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.response));
+                    getActivity().startService(intent);
                 }
             });
         });
