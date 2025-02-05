89c636dd02bb3356708215061e06582fb34b76e8
Patrick <binary_thinker@yahoo.com>
2025-02-04 16:16:15 -0800

    Service is working, refactoring.

--- app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
@@ -13,12 +13,10 @@ import androidx.lifecycle.ViewModelProvider;
 import edu.csueb.R;
 import edu.csueb.Service.DelayedMessageService;
 import edu.csueb.databinding.FragmentNotificationsBinding;
-import edu.csueb.ui.dashboard.DashboardViewModel;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
@@ -27,16 +25,17 @@ public class NotificationsFragment extends Fragment {
 
         View view = binding.getRoot();
 
+        Intent intent = new Intent(getActivity(), DelayedMessageService.class);
+        Log.i("log", "Inside of notificationsViewModel()");
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.response));
+        getActivity().startService(intent);
+
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), data -> {
             binding.serviceBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     binding.tvMessage.setText("Binding at work");
                     binding.serviceBtn.setText("Service Started");
-                    Intent intent = new Intent(getActivity(), DelayedMessageService.class);
-                    Log.i("log", "Inside of notificationsViewModel()");
-                    intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.response));
-                    getActivity().startService(intent);
                 }
             });
         });
