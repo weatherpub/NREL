04106a793e8b744e31fd8c8cd05917924227125f
Patrick <binary_thinker@yahoo.com>
2025-02-08 12:28:01 -0800

    Implementing Notifications...

--- app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/csueb/ui/notifications/NotificationsFragment.java
@@ -1,21 +1,25 @@
 package edu.csueb.ui.notifications;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.csueb.MainActivity;
 import edu.csueb.R;
 import edu.csueb.Service.DelayedMessageService;
 import edu.csueb.databinding.FragmentNotificationsBinding;
@@ -24,14 +28,39 @@ public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
+    private void createNotificationChannel() {
+        /*
+         * Create the NotificationChannel, but only on API 26+
+         * because the NotificationChannel class is not inn the Support Library.
+         */
 
+        Log.i("LOG", "createNotificationChannel method called");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel("sample", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getActivity().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
 
         View view = binding.getRoot();
 
         Intent intent = new Intent(getActivity(), DelayedMessageService.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Log.i("log", "Inside of notificationsViewModel()");
         intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getResources().getString(R.string.response));
         getActivity().startService(intent);
@@ -46,6 +75,23 @@ public class NotificationsFragment extends Fragment {
              .setVibrate      > Wait for 0 milliseconds before vibrating the device for 1000 milliseconds
              .setAutoCancel   > This makes the notification disappear when the user clicks on it.
          */
+//        Intent actionIntent = new Intent(getActivity(), MainActivity.class);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "sample")
+                .setAutoCancel(true)                               // .setAutoCancel   > This makes the notification disappear when the user clicks on it.
+                .setContentText("YouTube")
+                .setContentTitle(getString(R.string.question))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setVibrate(new long[] {0, 1000})                   // .setVibrate      > Wait for 0 milliseconds before vibrating the device for 1000 milliseconds
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit on one line, but multiple lines of text.")
+                )
+                .setContentIntent(pendingIntent);
+
+        createNotificationChannel();
+
+       /*
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity())
                 .setSmallIcon(android.R.drawable.sym_def_app_icon)  // .setSmallIcon    > Display a small icon in the upper-left most corner.
                 .setContentTitle(getString(R.string.question))      // .setContentTitle > Set the title
@@ -53,10 +99,18 @@ public class NotificationsFragment extends Fragment {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)      // .setPriority     > Make it  a high priority and vibrate the device.
                 .setVibrate(new long[] {0, 1000})                   // .setVibrate      > Wait for 0 milliseconds before vibrating the device for 1000 milliseconds
                 .setAutoCancel(true);                               // .setAutoCancel   > This makes the notification disappear when the user clicks on it.
+        */
 
-        Intent actionIntent = new Intent(getActivity(), MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContent(pendingIntent);
+        /* Add an action to tell the notification which activity to start when clicked
+         * */
+
+
+        /* Issue the notification using the built-in notification service
+         */
+
+        /*                      STOPPED HERE 02-07-25 7:34PM                    */
+
+        // NotificationManager notificationManager = getSystemService(NOTIFICATION_SERVICE);
 
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), data -> {
             binding.serviceBtn.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +118,6 @@ public class NotificationsFragment extends Fragment {
                 public void onClick(View view) {
                     binding.tvMessage.setText("Binding at work");
                     binding.serviceBtn.setText("Service Started");
-
                 }
             });
         });
